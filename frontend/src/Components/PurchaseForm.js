ffa37d042d1a80de976ca1ca1d7da34f3511bc6f
Stephen Keating <stephen.keating@gmail.com>
2020-02-14 21:57:40 -0500

    calculating purchase correctly

--- frontend/src/Components/PurchaseForm.js
+++ frontend/src/Components/PurchaseForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import userActions from '../redux/actions';
 
 const PurchaseForm = props => {
@@ -8,64 +9,91 @@ const PurchaseForm = props => {
   // Setting up local state using the useState hook
   const [quoteForm, setQuoteForm] = useState({
     ticker: '',
-    quantity: 0
+    quantity: 0,
+    latestPrice: 0,
+    showQuote: false
   });
+
+  // getting user balance from redux
+  const balance = useSelector(state => parseFloat(state.user.balance));
 
   const handleQuoteSubmit = e => {
     e.preventDefault();
-    userActions.getQuote(quoteForm)
-    .then(data => {
-      if(!data.symbol) {
-        console.log(data);
-        alert("Incorrect Ticker");
-        return;
-      } else {
-        console.log(data)
+    console.log(quoteForm);
+    if (quoteForm.quantity > 0) {
+      userActions.getQuote(quoteForm)
+      .then(data => {
+        if(!data.symbol) {
+          console.log(data);
+          alert("Incorrect Ticker");
+          return;
+        } else {
+          console.log(data)
+          saveQuote(data)
+        };
+      })
+    } else {
+      alert('Quantity must be 1 or more.');
     };
-    // .then(data => {
-    //   if(!data) {
-    //       // console.log(data.errors);
-    //       alert(data.errors);
-    //       return;
-    //   } else {
-    //     console.log(data)
-    //     dispatch(userActions.setUserAction(data.user));
-    //     dispatch(userActions.setTransactionsAction(data.transactions));
-    //     localStorage.setItem('token', data.token);
-    //     props.history.push('/');
-    //   }
-    })
   };
 
+  const handleBuyShares = e => {
+    e.preventDefault();
+    console.log(e.target.quotePrice.value, balance);
+    if ( e.target.quotePrice.value > balance ) {
+      alert('Insufficient Funds')
+    } else {
+      // console.log( ticker, quantity, quotePrice );
+      console.log( quoteForm, balance );
+    }
+  }
+
+  const saveQuote = data =>
+    setQuoteForm({ ...quoteForm, latestPrice: data.latestPrice, showQuote: true});
+
   const handleTickerChange = e =>
-    setQuoteForm({ ...quoteForm, [e.target.name]: e.target.value.toUpperCase() });
+    setQuoteForm({ ...quoteForm, [e.target.name]: e.target.value.toUpperCase(), showQuote: false });
   
-    const handleQuantityChange = e =>
-    setQuoteForm({ ...quoteForm, [e.target.name]: e.target.value });
+  const handleQuantityChange = e =>
+    setQuoteForm({ ...quoteForm, [e.target.name]: Math.round(e.target.value), showQuote: false });
 
   // Destructuring keys from our local state to use in the form
-  const { ticker, quantity } = quoteForm;
+  const { ticker, quantity, showQuote, latestPrice } = quoteForm;
 
   // Component code
   return (
-    <form onSubmit={handleQuoteSubmit}>
-      <input
-        type="text"
-        name="ticker"
-        value={ticker}
-        onChange={handleTickerChange}
-        placeholder="Ticker"
-      />
-      <input
-        type="number"
-        name="quantity"
-        value={quantity}
-
-        onChange={handleQuantityChange}
-        placeholder="Quantity"
-      />
-      <input type="submit" value='Get Quote'/>
-    </form>
+    <>
+      <form onSubmit={handleQuoteSubmit}>
+        <input
+          type="text"
+          name="ticker"
+          value={ticker}
+          onChange={handleTickerChange}
+          placeholder="Ticker"
+        />
+        <input
+          type="number"
+          name="quantity"
+          value={quantity}
+          onChange={handleQuantityChange}
+          placeholder="Quantity"
+        />
+        <input type="submit" value='Get Quote'/>
+      </form>
+      { showQuote
+        ? <form onSubmit={handleBuyShares}>
+            Price:
+            <input
+              type="number"
+              name="quotePrice"
+              readOnly
+              value={(latestPrice * quantity)}
+            />
+            <input type="submit" value='Buy Shares'/>
+          </form>
+        : null
+      }
+    </>
   );
 };
 
